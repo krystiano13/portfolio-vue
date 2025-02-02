b64fb5511bdac5f82577e85f20ea6bb724442b04
krystiano13 <crystianotv@gmail.com>
2025-02-02 13:54:09 +0100

    implement Hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,20 @@
+<script setup>
+  import Button from "primevue/button";
+</script>
+
+<template>
+  <section class="w-full flex flex-col items-center gap-7 p-4 pt-8">
+    <h1 class="text-6xl max-w-xl text-center font-semibold">Krystian Zieja</h1>
+    <h2 class="text-2xl max-w-xl text-center font-thin">Fullstack and Indie Game Developer Fullstack and Indie Game Developer Fullstack and Indie Game Developer Fullstack and Indie Game Developer </h2>
+    <div class="flex items-center justify-center gap-6">
+      <Button class="flex align-center gap-3">
+        <div>Read More</div>
+        <i class="pi pi-search"></i>
+      </Button>
+      <Button class="flex align-center gap-3" severity="secondary">
+        <div>Show CV</div>
+        <i class="pi pi-user"></i>
+      </Button>
+    </div>
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,7 @@
 <script setup>
-  import Card from "primevue/card";
-  import Button from "primevue/button";
+  import Hero from "@/components/Hero.vue";
 </script>
 
 <template>
-  <Card class="m-4">
-    <template #title>
-      <h1 class="text-4xl">Hello World</h1>
-    </template>
-    <template #content>
-      <Button class="!text-slate-50 !font-medium">
-          Hello
-          <i class="pi pi-check"></i>
-      </Button>
-    </template>
-  </Card>
+  <Hero />
 </template>
