38aafdcf2527f3e011057ad23d2a2783330f4fa0
krystiano13 <crystianotv@gmail.com>
2025-02-03 12:44:21 +0100

    implement desktop version of navbar

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -12,24 +12,26 @@
 </script>
 
 <template>
-  <section class="w-full h-[100vh] flex flex-col items-center justify-center gap-7 p-4 pt-8">
-    <h1 class="text-6xl max-w-xl text-center font-semibold">
-      Hi, I'm
-      <span class="text-emerald-500">Krystian Zieja</span>
-    </h1>
-    <h2 class="text-2xl max-w-xl text-center font-thin">
-      Fullstack and Indie Game Developer
-      <span :class="underscore ? `opacity-100` : `opacity-0`">_</span>
-    </h2>
-    <div class="flex items-center justify-center gap-6">
-      <Button class="flex align-center gap-3">
-        <div>Read More</div>
-        <i class="pi pi-search"></i>
-      </Button>
-      <Button class="flex align-center gap-3" severity="secondary">
-        <div>Show CV</div>
-        <i class="pi pi-user"></i>
-      </Button>
-    </div>
-  </section>
+  <div class="w-full h-[100vh] relative">
+    <section class="absolute w-full h-full flex flex-col items-center justify-center gap-7 p-4 pt-8">
+      <h1 class="text-6xl max-w-xl text-center font-semibold">
+        Hi, I'm
+        <span class="text-emerald-500">Krystian Zieja</span>
+      </h1>
+      <h2 class="text-2xl max-w-xl text-center font-thin">
+        Fullstack and Indie Game Developer
+        <span :class="underscore ? `opacity-100` : `opacity-0`">_</span>
+      </h2>
+      <div class="flex items-center justify-center gap-6">
+        <Button class="flex align-center gap-3">
+          <div>Read More</div>
+          <i class="pi pi-search"></i>
+        </Button>
+        <Button class="flex align-center gap-3" severity="secondary">
+          <div>Show CV</div>
+          <i class="pi pi-user"></i>
+        </Button>
+      </div>
+    </section>
+  </div>
 </template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="w-full p-5 font-medium text-lg flex items-center gap-4 fixed z-10 backdrop-blur-lg  bg-gray-900/10">
+    <a href="#home">
+      Home
+    </a>
+    <a href="#projects">
+      Projects
+    </a>
+    <a href="#skills">
+      Skills
+    </a>
+    <a href="#contact">
+      Contact
+    </a>
+  </nav>
+</template>
+
+<style scoped>
+  nav {
+    a::after {
+      width: 100%;
+      height: 2px;
+      content: "";
+      display: block;
+      top: 0;
+      left: 0;
+      background: whitesmoke;
+      transform: scaleX(0);
+      transition: transform 250ms;
+      will-change: transform;
+    }
+
+    a:hover::after {
+      transform: scaleX(1);
+    }
+  }
+</style>
