484847648ad2a47cef5c41196f68c03f91777c38
krystiano13 <crystianotv@gmail.com>
2025-02-03 13:01:45 +0100

    create hamburger button in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,21 +1,28 @@
 <script setup>
-
+  import Button from "primevue/button";
 </script>
 
 <template>
-  <nav class="w-full p-5 font-medium text-lg flex items-center gap-4 fixed z-10 backdrop-blur-lg  bg-gray-900/10">
-    <a href="#home">
-      Home
-    </a>
-    <a href="#projects">
-      Projects
-    </a>
-    <a href="#skills">
-      Skills
-    </a>
-    <a href="#contact">
-      Contact
-    </a>
+  <nav class="w-full p-5 font-medium text-lg fixed z-10 backdrop-blur-lg  bg-gray-900/10">
+   <section class="hidden sm:flex items-center gap-4">
+     <a href="#home">
+       Home
+     </a>
+     <a href="#projects">
+       Projects
+     </a>
+     <a href="#skills">
+       Skills
+     </a>
+     <a href="#contact">
+       Contact
+     </a>
+   </section>
+   <section class="flex items-center justify-end sm:hidden">
+      <Button class="w-12 h-12">
+        <i class="pi pi-bars"></i>
+      </Button>
+   </section>
   </nav>
 </template>
 
