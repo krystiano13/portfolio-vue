dca2779c65399f585f4d773d9b4d215c46c09664
krystiano13 <crystianotv@gmail.com>
2025-02-03 13:49:33 +0100

    implement work experience component

---
+++ src/components/Work.vue
@@ -0,0 +1,48 @@
+<script setup>
+  import { reactive } from "vue";
+  import Timeline from "primevue/timeline";
+  import Card from "primevue/card";
+
+  const data = reactive({
+    jobs: [
+      {
+        title: "Junior React Developer",
+        company: "Timenotes.io",
+        time: "Aug 2024 - Oct 2024",
+        about: "Creating new Timenotes' landing page with Next.js, TailwindCSS and Sanity.io .Working on Timenotes' React app"
+      },
+      {
+        title: "Intern Fullstack Developer",
+        company: "BeCode",
+        time: "Feb 2025 - Now",
+        about: "Learning how to program with technologies such as Symfony and Vue"
+      }
+    ]
+  });
+</script>
+
+<template>
+  <div class="w-full h-auto pb-16 flex flex-col justify-center items-center">
+    <h1 class="text-center text-4xl font-semibold mb-12">Work Experience</h1>
+    <section class="w-full flex justify-center items-center">
+      <Timeline class="max-w-4xl" align="alternate" :value="data.jobs">
+        <template #content="props">
+          <Card>
+            <template #title>
+              <h2 class="text-xl sm:text-2xl">{{ props.item.title }}</h2>
+              <h3 class="text-lg sm:text-xl font-regular">{{ props.item.company }}</h3>
+              <p class="text-sm sm:text-base font-light">{{ props.item.time }}</p>
+            </template>
+            <template #content>
+              <p>{{ props.item.about }}</p>
+            </template>
+          </Card>
+        </template>
+      </Timeline>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
   import Hero from "@/components/Hero.vue";
   import Navbar from "@/components/Navbar.vue";
+  import Work from "@/components/Work.vue";
 </script>
 
 <template>
   <Navbar />
   <Hero />
+  <Work />
 </template>
